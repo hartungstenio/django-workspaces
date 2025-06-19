c8577dc416bc5623e5ff45d0f78dba88ab539ef8
Christian Hartung <hartung@live.com>
2025-06-19 14:36:51 -0300

    chore: enable ruff

---
+++ src/django_workspaces/__init__.py
@@ -0,0 +1,5 @@
+"""Django reusable app to manage user workspaces.
+
+'Workspace' in this package is a unit of work. Each session always has one
+active workspace. Anything can be added to a workspace.
+"""
